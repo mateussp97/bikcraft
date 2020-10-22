27a41b6e88225c173c6508869c89124dcdf46ccf
Mateus Souza <mateusdesouzapaula97@gmail.com>
2020-10-22 20:45:39 -0300

    add: simple-anime.js upgrade: animating all html files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-new SimpleSlide({
-  slide: 'quote',
-  time: 5000,
-});
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'quote',
+    time: 5000,
+  });
 
-new SimpleSlide({
-  slide: 'portfolio',
-  time: 5000,
-  nav: true,
-  auto: true,
-  pauseOnHover: true,
-});
+  new SimpleSlide({
+    slide: 'portfolio',
+    time: 5000,
+    nav: true,
+    auto: true,
+    pauseOnHover: true,
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
